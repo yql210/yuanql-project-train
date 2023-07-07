40192fa8c477d8df7db8d31c1ed3235507e137c5
yuanql <56667999+yql210@users.noreply.github.com>
2023-07-07 21:07:19 +0800

    4-5 实现日志的相关配置
    修改启动日志，增加/member，修改logback日志

--- member/src/main/java/top/yuanql/train/member/conf/MembernApplication.java
+++ member/src/main/java/top/yuanql/train/member/conf/MembernApplication.java
@@ -1,15 +1,27 @@
 package top.yuanql.train.member.conf;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ConfigurableApplicationContext;
 import org.springframework.context.annotation.ComponentScan;
+import org.springframework.core.env.ConfigurableEnvironment;
+
 
 @SpringBootApplication
-@ComponentScan("top.yuanql.train.member")
+@ComponentScan("top.yuanql.train.member") // 配置包扫描路径。默认扫描此Java文件所在目录以及其目录下是所有文件，可以通过此命令修改默认扫描的路径
 public class MembernApplication {
 
+    private static final Logger LOG = LoggerFactory.getLogger(MembernApplication.class);
     public static void main(String[] args) {
-        SpringApplication.run(MembernApplication.class, args);
+        ConfigurableApplicationContext run = SpringApplication.run(MembernApplication.class, args);
+
+        // 添加启动日志
+        ConfigurableEnvironment environment = run.getEnvironment();
+        LOG.info("启动成功！！！！");
+        LOG.info("地址：\thttp://127.0.0.1:{}/member", environment.getProperty("server.port"));
+
     }
 
 }
