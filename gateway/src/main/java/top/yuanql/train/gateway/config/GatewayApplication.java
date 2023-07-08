a0422528e4d0a54a6d2e3ebf977d5e75adb9fcc3
yuanql <56667999+yql210@users.noreply.github.com>
2023-07-08 14:05:50 +0800

    # 4-9 详解项目中增加网关模块 (15:00)
    增加网关模块

--- gateway/src/main/java/top/yuanql/train/gateway/config/GatewayApplication.java
+++ gateway/src/main/java/top/yuanql/train/gateway/config/GatewayApplication.java
@@ -1,4 +1,4 @@
-package top.yuanql.train.member.conf;
+package top.yuanql.train.gateway.config;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -11,16 +11,16 @@ import org.springframework.core.env.ConfigurableEnvironment;
 
 @SpringBootApplication
 @ComponentScan("top.yuanql.train") // 配置包扫描路径。默认扫描此Java文件所在目录以及其目录下是所有文件，可以通过此命令修改默认扫描的路径
-public class MembernApplication {
+public class GatewayApplication {
 
-    private static final Logger LOG = LoggerFactory.getLogger(MembernApplication.class);
+    private static final Logger LOG = LoggerFactory.getLogger(GatewayApplication.class);
     public static void main(String[] args) {
-        ConfigurableApplicationContext run = SpringApplication.run(MembernApplication.class, args);
+        ConfigurableApplicationContext run = SpringApplication.run(GatewayApplication.class, args);
 
         // 添加启动日志
         ConfigurableEnvironment environment = run.getEnvironment();
         LOG.info("启动成功！！！！");
-        LOG.info("地址：\thttp://127.0.0.1:{}{}", environment.getProperty("server.port"), environment.getProperty("server.servlet.context-path"));
+        LOG.info("地址：\thttp://127.0.0.1:{}", environment.getProperty("server.port"));
 
     }
 
