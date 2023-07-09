6169e2b23e0939d14041d0714c5f34ef6b367475
yuanql <56667999+yql210@users.noreply.github.com>
2023-07-09 21:36:39 +0800

    # 4-17 为项目增加统一异常处理 (04:56)

---
+++ common/src/main/java/top/yuanql/train/common/controller/ControllerExceptionHandler.java
@@ -0,0 +1,34 @@
+package top.yuanql.train.common.controller;
+
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseBody;
+import top.yuanql.train.common.response.CommonResp;
+
+/**
+ * 统一异常处理、数据预处理等
+ */
+@ControllerAdvice   // 异常拦截器的注解
+public class ControllerExceptionHandler {
+
+    private static final Logger LOG = LoggerFactory.getLogger(ControllerExceptionHandler.class);
+
+    /**
+     * 所有异常统一处理
+     * @param e
+     * @return
+     */
+    @ExceptionHandler(value = Exception.class)
+    @ResponseBody
+    public CommonResp exceptionHandler(Exception e) throws Exception {
+        CommonResp commonResp = new CommonResp();
+        LOG.error("系统异常：", e);
+        commonResp.setSuccess(false);
+        commonResp.setMessage("系统出现异常，请联系管理员： " + e.getMessage());
+        return commonResp;
+    }
+
+}
