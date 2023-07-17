6c6e26da9fec53776577fac4aeb08439dbd32613
yuanql <56667999+yql210@users.noreply.github.com>
2023-07-17 20:41:53 +0800

    # 5-12 增加web控台主页 (10:42)
    增加控制台主页

--- web/src/router/index.js
+++ web/src/router/index.js
@@ -1,21 +1,15 @@
 import { createRouter, createWebHistory } from 'vue-router'
-import HomeView from '../views/login.vue'
 
 const routes = [
-  {
-    path: '/',
-    name: 'home',
-    component: HomeView
-  },
-  {
-    path: '/about',
-    name: 'about',
-    component: () => import('../views/AboutView.vue')
-  },
   {
     path: '/login',
     name: 'login',
     component: () => import('../views/login.vue')
+  },
+  {
+    path: '/',
+    name: 'main',
+    component: () => import('../views/main.vue')
   }
 ]
 
--- web/src/views/AboutView.vue
+++
@@ -1,6 +0,0 @@
-<template>
-  <div class="about">
-    <h1>This is an about page</h1>
-    <a-button>选择</a-button><arrow-down-outlined />
-  </div>
-</template>
