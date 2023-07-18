461e0372f64b75027ddee6c69ab80c645cbc0245
yuanql <56667999+yql210@users.noreply.github.com>
2023-07-18 19:39:11 +0800

    # 5-13 制作Vue3公共组件 (12:37)
    vue组件开发，增加the-sider组件

--- web/src/views/main.vue
+++ web/src/views/main.vue
@@ -3,51 +3,7 @@
     <train-the-header-view></train-the-header-view>
     <a-layout-content style="padding: 0 50px">
       <a-layout style="padding: 24px 0; background: #fff">
-        <a-layout-sider width="200" style="background: #fff">
-          <a-menu
-              v-model:selectedKeys="selectedKeys2"
-              v-model:openKeys="openKeys"
-              mode="inline"
-              style="height: 100%"
-          >
-            <a-sub-menu key="sub1">
-              <template #title>
-                <span>
-                  <user-outlined />
-                  subnav 1
-                </span>
-              </template>
-              <a-menu-item key="1">option1</a-menu-item>
-              <a-menu-item key="2">option2</a-menu-item>
-              <a-menu-item key="3">option3</a-menu-item>
-              <a-menu-item key="4">option4</a-menu-item>
-            </a-sub-menu>
-            <a-sub-menu key="sub2">
-              <template #title>
-                <span>
-                  <laptop-outlined />
-                  subnav 2
-                </span>
-              </template>
-              <a-menu-item key="5">option5</a-menu-item>
-              <a-menu-item key="6">option6</a-menu-item>
-              <a-menu-item key="7">option7</a-menu-item>
-              <a-menu-item key="8">option8</a-menu-item>
-            </a-sub-menu>
-            <a-sub-menu key="sub3">
-              <template #title>
-                <span>
-                  <notification-outlined />
-                  subnav 3
-                </span>
-              </template>
-              <a-menu-item key="9">option9</a-menu-item>
-              <a-menu-item key="10">option10</a-menu-item>
-              <a-menu-item key="11">option11</a-menu-item>
-              <a-menu-item key="12">option12</a-menu-item>
-            </a-sub-menu>
-          </a-menu>
-        </a-layout-sider>
+        <train-the-sider-view></train-the-sider-view>
         <a-layout-content :style="{ padding: '0 24px', minHeight: '280px' }">
           <a-breadcrumb style="margin: 16px 0">
             <a-breadcrumb-item>Home</a-breadcrumb-item>
@@ -67,10 +23,11 @@
 <script>
 import {defineComponent} from "vue";
 import TrainTheHeaderView from "@/components/the-header.vue";
+import TrainTheSiderView from "@/components/the-sider.vue";
 
 export default defineComponent({
   name: "train-main-view",
-  components: {TrainTheHeaderView}
+  components: {TrainTheSiderView, TrainTheHeaderView}
 });
 </script>
 
