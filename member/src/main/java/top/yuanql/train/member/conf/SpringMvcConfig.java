322607306682edfa3a144c009327c609a3c499f5
yuanql <56667999+yql210@users.noreply.github.com>
2023-07-20 22:22:57 +0800

    # 7-5 使用线程本地变量存储会员信息 (20:36)
    增加日志拦截器，统一打印日志流水号

--- member/src/main/java/top/yuanql/train/member/conf/SpringMvcConfig.java
+++ member/src/main/java/top/yuanql/train/member/conf/SpringMvcConfig.java
@@ -5,16 +5,21 @@ import jakarta.annotation.Resource;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import top.yuanql.train.common.interceptor.LogInterceptor;
 import top.yuanql.train.common.interceptor.MemberInterceptor;
 
 @Configuration
 public class SpringMvcConfig implements WebMvcConfigurer {
 
    @Resource
+   LogInterceptor logInterceptor;
+
+   @Resource
    MemberInterceptor memberInterceptor;
 
    @Override
    public void addInterceptors(InterceptorRegistry registry) {
+       registry.addInterceptor(logInterceptor);
 
        // 路径不要包含context-path
        registry.addInterceptor(memberInterceptor)
