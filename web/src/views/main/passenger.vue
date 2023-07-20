2d738fda92b4236686f7b38b3dd2001da00f0fe1
yuanql <56667999+yql210@users.noreply.github.com>
2023-07-20 22:41:14 +0800

    # 7-6 前端二级路由页面开发 (19:23)
    增加乘车人管理页面

---
+++ web/src/views/main/passenger.vue
@@ -0,0 +1,16 @@
+<template>
+ <h1>乘车人管理</h1>
+</template>
+
+<script>
+
+import { defineComponent } from 'vue';
+
+export default defineComponent({
+  name: "train-passenger-view",
+  setup() {
+    return {
+    };
+  },
+});
+</script>
