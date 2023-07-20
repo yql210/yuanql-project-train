2f189c56f6388b6383f53fc6bf6edc0ffc13830e
yuanql <56667999+yql210@users.noreply.github.com>
2023-07-20 17:01:52 +0800

    # 6-8 为gateway增加登录校验拦截器 (14:21)
    为gateway增加登录校验过滤器

---
+++ gateway/src/main/java/top/yuanql/train/gateway/config/LoginMemberFilter.java
@@ -0,0 +1,69 @@
+package top.yuanql.train.gateway.config;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.cloud.gateway.filter.GatewayFilterChain;
+import org.springframework.cloud.gateway.filter.GlobalFilter;
+import org.springframework.core.Ordered;
+import org.springframework.http.HttpStatus;
+import org.springframework.stereotype.Component;
+import org.springframework.web.server.ServerWebExchange;
+import reactor.core.publisher.Mono;
+import top.yuanql.train.gateway.config.util.JWTUtilMy;
+
+/**
+ * @BelongsProject: yuanql-project-train
+ * @BelongsPackage: top.yuanql.train.gateway.config
+ * @BelongsClassName: LoginMemberFilter
+ * @Author: yuanql
+ * @CreateTime: 2023-07-20  15:31
+ * @Description: 拦截器
+ * @Version: 1.0
+ */
+
+@Component
+public class LoginMemberFilter implements GlobalFilter, Ordered {
+
+    private static final Logger LOG = LoggerFactory.getLogger(LoginMemberFilter.class);
+
+    @Override
+    public Mono<Void> filter(ServerWebExchange exchange, GatewayFilterChain chain) {
+        String path = exchange.getRequest().getURI().getPath();
+
+        // 排除不需要拦截的请求
+        if (path.contains("/admin")
+                || path.contains("/hello")
+                || path.contains("member/member/login")
+                || path.contains("/member/member/send-code")) {
+            LOG.info("不需要登录验证：{}", path);
+            return chain.filter(exchange);
+        }
+
+        LOG.info("需要登录验证：{}", path);
+
+        // 获取header的token参数
+        String token = exchange.getRequest().getHeaders().getFirst("token");
+        LOG.info("会员登录验证开始，token：{}", token);
+        if (token == null || token.isEmpty()) {
+            LOG.info("token为空，请求被拦截");
+            exchange.getResponse().setStatusCode(HttpStatus.UNAUTHORIZED);
+            return exchange.getResponse().setComplete();
+        }
+
+        // 验证token是否有效，包括token是否被改过，是否过期
+        boolean validate = JWTUtilMy.validate(token);
+        if (validate) {
+            LOG.info("token有效，放行该请求");
+            return chain.filter(exchange);
+        } else {
+            LOG.info("token无效，请求被拦截");
+            exchange.getResponse().setStatusCode(HttpStatus.UNAUTHORIZED);
+            return exchange.getResponse().setComplete();
+        }
+    }
+
+    @Override
+    public int getOrder() {
+        return 0; // 用于拍过滤器的顺序
+    }
+}
--- gateway/src/main/java/top/yuanql/train/gateway/config/Test1Filer.java
+++ gateway/src/main/java/top/yuanql/train/gateway/config/Test1Filer.java
@@ -1,37 +1,37 @@
-package top.yuanql.train.gateway.config;
-
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-import org.springframework.cloud.gateway.filter.GatewayFilterChain;
-import org.springframework.cloud.gateway.filter.GlobalFilter;
-import org.springframework.core.Ordered;
-import org.springframework.stereotype.Component;
-import org.springframework.web.server.ServerWebExchange;
-import reactor.core.publisher.Mono;
-
-/**
- * @BelongsProject: yuanql-project-train
- * @BelongsPackage: top.yuanql.train.gateway.config
- * @BelongsClassName: Test1Filer
- * @Author: yuanql
- * @CreateTime: 2023-07-20  15:31
- * @Description: 拦截器
- * @Version: 1.0
- */
-
-@Component
-public class Test1Filer implements GlobalFilter, Ordered {
-
-    private static final Logger LOG = LoggerFactory.getLogger(Test1Filer.class);
-
-    @Override
-    public Mono<Void> filter(ServerWebExchange exchange, GatewayFilterChain chain) {
-        LOG.info("Test1Filer");
-        return chain.filter(exchange);
-    }
-
-    @Override
-    public int getOrder() {
-        return 0; // 用于拍过滤器的顺序
-    }
-}
+//package top.yuanql.train.gateway.config;
+//
+//import org.slf4j.Logger;
+//import org.slf4j.LoggerFactory;
+//import org.springframework.cloud.gateway.filter.GatewayFilterChain;
+//import org.springframework.cloud.gateway.filter.GlobalFilter;
+//import org.springframework.core.Ordered;
+//import org.springframework.stereotype.Component;
+//import org.springframework.web.server.ServerWebExchange;
+//import reactor.core.publisher.Mono;
+//
+///**
+// * @BelongsProject: yuanql-project-train
+// * @BelongsPackage: top.yuanql.train.gateway.config
+// * @BelongsClassName: Test1Filer
+// * @Author: yuanql
+// * @CreateTime: 2023-07-20  15:31
+// * @Description: 拦截器
+// * @Version: 1.0
+// */
+//
+//@Component
+//public class Test1Filer implements GlobalFilter, Ordered {
+//
+//    private static final Logger LOG = LoggerFactory.getLogger(Test1Filer.class);
+//
+//    @Override
+//    public Mono<Void> filter(ServerWebExchange exchange, GatewayFilterChain chain) {
+//        LOG.info("Test1Filer");
+//        return chain.filter(exchange);
+//    }
+//
+//    @Override
+//    public int getOrder() {
+//        return 0; // 用于拍过滤器的顺序
+//    }
+//}
