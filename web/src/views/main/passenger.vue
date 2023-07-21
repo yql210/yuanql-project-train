0f4738f46e639f4315980a0b516720b3901dd17d
yuanql <56667999+yql210@users.noreply.github.com>
2023-07-21 12:29:46 +0800

    # 7-12 乘车人列表查询界面开发（二） (15:03)
    显示分页页码

--- web/src/views/main/passenger.vue
+++ web/src/views/main/passenger.vue
@@ -2,7 +2,7 @@
   <p>
     <a-button type="primary" @click="showModal">新增</a-button>
   </p>
-  <a-table :data-source="passengers" :columns="colums"/>
+  <a-table :data-source="passengers" :columns="colums" :pagination="pagination"/>
   <a-modal v-model:visible="visible" title="乘车人" @ok="handleOk"
       ok-text="确认" cancel-text="取消">
     <a-form :model="passenger" :label-col="{span: 4}" :wrapper-col="{ span: 20 }">
@@ -44,6 +44,13 @@ export default defineComponent({
     });
 
     const passengers = ref([]);
+
+    // 分页的三个属性名是固定的
+    const pagination = reactive({
+      total: 0,
+      current: 1,
+      pageSize: 2,
+    });
 
     const colums = [ {
       title: '姓名',
@@ -87,6 +94,7 @@ export default defineComponent({
         let data = response.data;
         if (data.success) {
           passengers.value = data.content.list;
+          pagination.total = data.content.total;
         } else {
           notification.error({description: data.message});
         }
@@ -107,7 +115,7 @@ export default defineComponent({
       handleOk,
       passengers,
       colums,
-
+      pagination,
     };
   },
 });
