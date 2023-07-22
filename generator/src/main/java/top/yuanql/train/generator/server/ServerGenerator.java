8d97d79472c247c03957a3ecb86f6251d5b51dd7
yuanql <56667999+yql210@users.noreply.github.com>
2023-07-22 17:15:11 +0800

    # 8-10 详解vue界面生成器 (18:03)
    制作vue.ftl模板，支持只读

--- generator/src/main/java/top/yuanql/train/generator/server/ServerGenerator.java
+++ generator/src/main/java/top/yuanql/train/generator/server/ServerGenerator.java
@@ -15,6 +15,8 @@ import java.util.*;
 
 public class ServerGenerator {
 
+    static boolean readOnly = false;
+    static String vuePath = "web/src/views/main/";
     static String serverPath = "[module]/src/main/java/top/yuanql/train/[module]/";
 
     static String pomPath = "generator/pom.xml";
@@ -78,6 +80,7 @@ public class ServerGenerator {
         param.put("tableNameCn", tableNameCn);
         param.put("fieldList", fieldList);
         param.put("typeSet", typeSet);
+        param.put("readOnly", readOnly);
         System.out.println("param = " + param);
 
 //        gen(Domain, param, "service/Impl", "serviceImpl");
@@ -85,8 +88,17 @@ public class ServerGenerator {
 //        gen(Domain, param, "controller", "controller");
 //        gen(Domain, param, "req", "saveReq");
 //        gen(Domain, param, "req", "queryReq");
-        gen(Domain, param, "response", "queryResp");
+//        gen(Domain, param, "response", "queryResp");
 
+        genVue(do_main, param);
+    }
+
+    private static void genVue(String do_main, Map<String, Object> param) throws IOException, TemplateException {
+        FreemarkerUtil.initConfig("vue.ftl");
+        new File(vuePath).mkdirs();
+        String fileName = vuePath + do_main + ".vue";
+        System.out.println("开始生成：" + fileName);
+        FreemarkerUtil.generator(fileName, param);
     }
 
     private static void gen(String Domain, Map<String, Object> param, String packageName, String target) throws IOException, TemplateException {
