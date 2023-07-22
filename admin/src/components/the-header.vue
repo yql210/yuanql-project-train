cca3fe2397e39b6138562691e9ddbe633af782e5
yuanql <56667999+yql210@users.noreply.github.com>
2023-07-22 18:37:27 +0800

    # 9-3 项目中增加admin控台模块 (13:45)
    admin去掉登录页面，去掉menber和登录相关的拦截，去掉乘车人页面

--- admin/src/components/the-header.vue
+++ admin/src/components/the-header.vue
@@ -2,10 +2,7 @@
   <a-layout-header class="header">
     <div class="logo" />
     <div style="float: right; color: white">
-      您好：{{member.mobile}} &nbsp;&nbsp;
-      <router-link to="/login" style="color: white">
-        退出登录
-      </router-link>
+      欢迎使用控制台
     </div>
     <a-menu
         v-model:selectedKeys="selectedKeys"
@@ -19,9 +16,9 @@
         </router-link>
       </a-menu-item>
 
-      <a-menu-item key="/passenger">
-        <router-link to="/passenger">
-          <user-outlined /> &nbsp;乘车人管理
+      <a-menu-item key="/about">
+        <router-link to="/about">
+          <user-outlined /> &nbsp;关于
         </router-link>
       </a-menu-item>
 
@@ -31,14 +28,12 @@
 
 <script>
 import {defineComponent, ref, watch} from 'vue';
-import store from "@/store";
 import router from "@/router";
 
 
 export default defineComponent({
   name: "train-the-header-view",  // 此处加入可以解决Vue页面报错的问题
   setup() {
-    let member = store.state.member;
     const selectedKeys = ref([]);
 
     watch(() => router.currentRoute.value.path, (newValue) => {
@@ -47,7 +42,6 @@ export default defineComponent({
       selectedKeys.value.push(newValue);
     }, {immediate: true});
     return {
-      member,
       selectedKeys
     };
   },
---
+++ admin/src/views/main/about.vue
@@ -0,0 +1,16 @@
+<template>
+  <h1>关于Yuanql 12306 售票系统</h1>
+</template>
+
+<script>
+
+import { defineComponent } from 'vue';
+
+export default defineComponent({
+  name: "train-welcome-view",
+  setup() {
+    return {
+    };
+  },
+});
+</script>
