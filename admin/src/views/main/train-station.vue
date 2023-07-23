616d36081cef637786d99f2e56be9545de34808b
yuanql <56667999+yql210@users.noreply.github.com>
2023-07-23 11:42:13 +0800

    # 9-14 制作车次下拉框组件（二） (09:58)
    火车车厢页面的车次改为车次选择组件

--- admin/src/views/main/train-station.vue
+++ admin/src/views/main/train-station.vue
@@ -28,7 +28,7 @@
            ok-text="确认" cancel-text="取消">
     <a-form :model="trainStation" :label-col="{span: 4}" :wrapper-col="{ span: 20 }">
       <a-form-item label="车次编号">
-        <train-select-view v-model:model-value="trainStation.trainCode" width="50%"> </train-select-view>
+        <train-select-view v-model:model-value="trainStation.trainCode"> </train-select-view>
       </a-form-item>
       <a-form-item label="站序">
         <a-input v-model:value="trainStation.index" />
@@ -219,41 +219,12 @@ export default defineComponent({
       });
     };
 
-    // -------------车次下拉框--------------
-    const trains = ref([]);
-
-    /**
-     * 查询所有车次用于车次下下拉框
-     */
-    const queryTrainCode = () => {
-      axios.get("/business/admin/train/query-all").then((response) => {
-        let data = response.data;
-        if (data.success) {
-          trains.value = data.content;
-        } else {
-          notification.error({description: data.message});
-        }
-      });
-    };
-
-    /**
-     * 车次下拉框筛选
-     * @param input
-     * @param option
-     * @returns {boolean}
-     */
-    const filterTrainCodeOption = (input, option) => {
-      console.log(input, option);
-      return option.label.toLowerCase().indexOf(input.toLowerCase()) >= 0;
-    }
-
     onMounted(() => {
       handleQuery({
         page: 1,
         size: pagination.value.pageSize
       });
 
-      queryTrainCode();
     });
 
     return {
@@ -269,8 +240,6 @@ export default defineComponent({
       handleOk,
       onEdit,
       onDelete,
-      trains,
-      filterTrainCodeOption
     };
   },
 });
