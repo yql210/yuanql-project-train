de8ef9948827528e0ace807677c1206e30a0e002
yuanql <56667999+yql210@users.noreply.github.com>
2023-07-23 09:57:07 +0800

    # 9-11 使用pinyin-pro将汉字转成拼音 (10:38)
    为火车和火车车站界面增加pinyin

--- admin/src/views/main/station.vue
+++ admin/src/views/main/station.vue
@@ -92,6 +92,9 @@ export default defineComponent({
       if (Tool.isNotEmpty(station.value.name)) {
         station.value.namePinyin = pinyin(station.value.name, { toneType: 'none'}).replaceAll(" ", "");
         station.value.namePy = pinyin(station.value.name, { pattern: 'first', toneType: 'none'}).replaceAll(" ", "");
+      } else {
+        station.value.namePinyin = "";
+        station.value.namePy = "";
       }
     }, {immediate: true});
 
