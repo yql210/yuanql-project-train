eaa8c59aab7234e2e90ce5e2d92e5225b8b6e2e2
yuanql <56667999+yql210@users.noreply.github.com>
2023-07-24 12:52:52 +0800

    # 9-19 完善车次&车厢&座位管理功能 (23:51)
    重新生成火车座位页面，只读

--- generator/src/main/java/top/yuanql/train/generator/gen/ServerGenerator.java
+++ generator/src/main/java/top/yuanql/train/generator/gen/ServerGenerator.java
@@ -15,7 +15,7 @@ import java.util.*;
 
 public class ServerGenerator {
 
-    static boolean readOnly = false;
+    static boolean readOnly = true;
     static String vuePath = "admin/src/views/main/";
     static String serverPath = "[module]/src/main/java/top/yuanql/train/[module]/";
 
@@ -83,12 +83,12 @@ public class ServerGenerator {
         param.put("readOnly", readOnly);
         System.out.println("param = " + param);
 
-        gen(Domain, param, "service/Impl", "serviceImpl");
-        gen(Domain, param, "service", "service");
-        gen(Domain, param, "controller/admin", "adminController");
-        gen(Domain, param, "req", "saveReq");
-        gen(Domain, param, "req", "queryReq");
-        gen(Domain, param, "response", "queryResp");
+//        gen(Domain, param, "service/Impl", "serviceImpl");
+//        gen(Domain, param, "service", "service");
+//        gen(Domain, param, "controller/admin", "adminController");
+//        gen(Domain, param, "req", "saveReq");
+//        gen(Domain, param, "req", "queryReq");
+//        gen(Domain, param, "response", "queryResp");
 
         genVue(do_main, param);
     }
