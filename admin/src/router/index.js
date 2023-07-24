e5ee86a71c348dd3d284d5e1cbd8691678cf707a
yuanql <56667999+yql210@users.noreply.github.com>
2023-07-24 17:43:53 +0800

    # 10-8 通过控台界面操作定时任务 (18:19)
    对菜单做分类，改为两级菜单；路由也改为多级路由

--- admin/src/router/index.js
+++ admin/src/router/index.js
@@ -12,30 +12,37 @@ const routes = [
     }, {
       path: 'about',
       component: () => import('../views/main/about.vue')
-    },
-      {
-      path: 'station',
-      component: () => import('../views/main/station.vue')
-    },
-       {
-      path: 'train',
-      component: () => import('../views/main/train.vue')
-    },
-      {
-      path: 'train-station',
-      component: () => import('../views/main/train-station.vue')
-    },
-       {
-      path: 'train-carriage',
-      component: () => import('../views/main/train-carriage.vue')
-    },
-       {
-      path: 'train-seat',
-      component: () => import('../views/main/train-seat.vue')
-    },
+    }, {
+      path: 'base/',
+      children: [{
+        path: 'station',
+        component: () => import('../views/main/station.vue')
+      },
         {
-      path: 'batch/job',
-      component: () => import('../views/main/job.vue')
+          path: 'train',
+          component: () => import('../views/main/train.vue')
+        },
+        {
+          path: 'train-station',
+          component: () => import('../views/main/train-station.vue')
+        },
+        {
+          path: 'train-carriage',
+          component: () => import('../views/main/train-carriage.vue')
+        },
+        {
+          path: 'train-seat',
+          component: () => import('../views/main/train-seat.vue')
+        },
+      ]
+    },{
+      path: 'batch/',
+      children: [
+        {
+          path: 'job',
+          component: () => import('../views/main/job.vue')
+        },
+      ]
     },
     ]
   },
