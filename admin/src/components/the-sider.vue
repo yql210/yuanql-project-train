7018da7e031388147268a7cdf6e0e004828d7c2c
yuanql <56667999+yql210@users.noreply.github.com>
2023-07-24 17:33:44 +0800

    # 10-8 通过控台界面操作定时任务 (18:19)
    通过界面操作定时任务

--- admin/src/components/the-sider.vue
+++ admin/src/components/the-sider.vue
@@ -41,6 +41,12 @@
         </router-link>
       </a-menu-item>
 
+      <a-menu-item key="/batch/job">
+        <router-link to="/batch/job">
+          <bars-outlined /> &nbsp;任务管理
+        </router-link>
+      </a-menu-item>
+
     </a-menu>
   </a-layout-sider>
 </template>
